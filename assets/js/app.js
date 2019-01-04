b9f09e7ebd1c421547b92c7edb765f1bfd085c29
Michael Salvati <print@3daddict.com>
2019-01-04 15:28:26 -0800

    stl files loading from dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,40 @@
  
-const benchy = '../stl/3DBenchy.stl';
+document.addEventListener("DOMContentLoaded", function(event) { 
+	selectStlFile() 
+  });
+
+const benchy = '../assets/stl/3DBenchy.stl';
 const squareAscii = '../assets/stl/square-ascii.STL';
 const squareBinary = '../assets/stl/square-binary.STL';
+let selectedStlFileSrc;
 
+function selectStlFile() {
+	const e = document.getElementById("stlList");
+	const stlFileSelected = e.options[e.selectedIndex].value;
+	console.log('STL FILE SELECTED: ', stlFileSelected);
 
-var loader = new THREE.STLLoader();
+	switch (stlFileSelected) {
+		case 'benchy':
+			selectedStlFileSrc = benchy;
+			console.log(selectedStlFileSrc)
+			break;
+		case 'cube-ascii':
+			selectedStlFileSrc = squareAscii;
+			console.log(selectedStlFileSrc)
+			break;
+		case 'cube-binary':
+			selectedStlFileSrc = squareBinary;
+			console.log(selectedStlFileSrc)
+			break;
+	
+		default:
+			break;
+	}
+
+	const loader = new THREE.STLLoader();
+
 //Target the input field
-const stlUpload = document.querySelector('#stlUpload');
-
-//Event listener on choose file
-stlUpload.addEventListener('change', function(e) {
-	console.log('event listener triggered');
-    //call file reader class
-	const reader = new FileReader();
-	reader.addEventListener( 'load', function ( event ) {
-		console.log('reader listener triggered');
-		var contents = event.target.result;
-
-		var geometry = new THREE.STLLoader().parse( contents );
-		geometry.sourceType = "stl";
-		geometry.sourceFile = file.name;
-
-		var material = new THREE.MeshStandardMaterial();
-
-		var mesh = new THREE.Mesh( geometry, material );
-		mesh.name = filename;
-
-		editor.execute( new AddObjectCommand( mesh ) );
-
-		console.log(contents);
-		console.log(geometry);
-		console.log(material);
-		console.log(mesh);
-
-	}, false);
-});
-
-loader.load(squareAscii, function(geometry) {
+loader.load(selectedStlFileSrc, function(geometry) {
   let calculatedStlVolume = Math.round(getVolume(geometry));
   let stlSize = getSize(geometry);
 
@@ -44,17 +42,15 @@ loader.load(squareAscii, function(geometry) {
   localStorage.setItem("stlVolume", calculatedStlVolume);
 
   let stlSizeOutput = {
-    x: stlSize.x,
-    y: stlSize.y,
-    z: stlSize.z
+	x: stlSize.x,
+	y: stlSize.y,
+	z: stlSize.z
   };
 
   document.querySelector('#stlSizeL').textContent = Math.round(stlSizeOutput.x);
   document.querySelector('#stlSizeW').textContent = Math.round(stlSizeOutput.y);
   document.querySelector('#stlSizeH').textContent = Math.round(stlSizeOutput.z);
-  localStorage.setItem("stlSizeL", stlSizeOutput.x);
-  localStorage.setItem("stlSizeW", stlSizeOutput.y);
-  localStorage.setItem("stlSizeH", stlSizeOutput.z);
+
 });
 
 // check with known volume:
@@ -72,13 +68,13 @@ function getVolume(geometry) {
   let faces = position.count / 3;
   let sum = 0;
   let p1 = new THREE.Vector3(),
-      p2 = new THREE.Vector3(),
-      p3 = new THREE.Vector3();
+	  p2 = new THREE.Vector3(),
+	  p3 = new THREE.Vector3();
   for (let i = 0; i < faces; i++) {
-    p1.fromBufferAttribute(position, i * 3 + 0);
-    p2.fromBufferAttribute(position, i * 3 + 1);
-    p3.fromBufferAttribute(position, i * 3 + 2);
-    sum += signedVolumeOfTriangle(p1, p2, p3);
+	p1.fromBufferAttribute(position, i * 3 + 0);
+	p2.fromBufferAttribute(position, i * 3 + 1);
+	p3.fromBufferAttribute(position, i * 3 + 2);
+	sum += signedVolumeOfTriangle(p1, p2, p3);
   }
   return sum;
 
@@ -94,4 +90,7 @@ function getSize(geometry) {
   geometry.boundingBox.getSize( size );
   return size;
 }
+}
+
+
 
